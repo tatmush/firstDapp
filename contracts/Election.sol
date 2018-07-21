7ad29471e14343a14f72b091e16c1b6c16ada5cb
tatmush <tatalmondmush@gmail.com>
2018-07-21 19:36:55 +0200

    phase 1 done

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+contract Election{
+	//Store a candidate
+	//read candidate
+	string public candidate;
+	//constructor
+	function Election() public {
+		candidate="Candidate 1";
+
+	}
+}
