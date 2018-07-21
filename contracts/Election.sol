31d6378551899e246d0271cb448e4622811f39fc
tatmush <tatalmondmush@gmail.com>
2018-07-21 23:59:28 +0200

    third stage

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -12,6 +12,9 @@ contract Election{
 	//fetch candidates
 	mapping (uint => Candidate) public candidates;
 
+	//store accounts that have voted
+	mapping (address => bool) public voters;
+
 	//store candidates count
 	uint public candidatesCount;
 
@@ -24,4 +27,18 @@ contract Election{
 		candidatesCount++;
 		candidates[candidatesCount]=Candidate(candidatesCount, _name, 0);
 	}
-}
+
+	function vote(uint _candidateId) public{
+		//require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		//require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		//record that voter has voted
+		voters[msg.sender]=true;
+
+		//update candidate vote count;
+		candidates[_candidateId].voteCount++;
+	}
+} 
