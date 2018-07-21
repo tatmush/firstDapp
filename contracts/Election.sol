d4e922b07ea2096681697c2c9d36b0bc33e4530c
tatmush <tatalmondmush@gmail.com>
2018-07-21 22:44:13 +0200

    if you face any problems try the chrome browser

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.24;
 
 contract Election{
-	//Store a candidate
-	//read candidate
-	string public candidate;
-	//constructor
-	function Election() public {
-		candidate="Candidate 1";
+	//Model a candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
 
+	//store candidates
+	//fetch candidates
+	mapping (uint => Candidate) public candidates;
+
+	//store candidates count
+	uint public candidatesCount;
+
+	function Election() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount]=Candidate(candidatesCount, _name, 0);
 	}
 }
